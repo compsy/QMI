46cc86eea003dccaf4a83b382d332d92e8357915
Pal Poshyachinda <palp@Pals-MacBook-Pro.local>
2020-03-29 01:18:38 +0700

    drag and drop merged

--- question-interface-managment/src/reducers/questionnaireReducer.js
+++ question-interface-managment/src/reducers/questionnaireReducer.js
@@ -1,7 +1,47 @@
 import uuid from "uuid/v1";
 
+const reorder = (list, startIndex, endIndex) => {
+  const [removed] = list.splice(startIndex, 1);
+  list.splice(endIndex, 0, removed);
+  return list;
+};
+
+const copy = (source, destination, droppableSource, droppableDestination) => {
+  const item = source[droppableSource.index];
+  destination.splice(droppableDestination.index, 0, {
+    id: uuid(),
+    type: item.label,
+    title: `untitled ${item.label}`,
+    options: ["option1", "option2", "option3", "option4"]
+  });
+  return destination;
+};
+
+const QUESTION_TYPES = [
+  { id: uuid(), label: "checkbox" },
+  { id: uuid(), label: "radio" },
+  { id: uuid(), label: "likert" },
+  { id: uuid(), label: "range" },
+  { id: uuid(), label: "raw" },
+  { id: uuid(), label: "textarea" },
+  { id: uuid(), label: "textfield" },
+  { id: uuid(), label: "number" },
+  { id: uuid(), label: "expandable" },
+  { id: uuid(), label: "time" },
+  { id: uuid(), label: "date" },
+  { id: uuid(), label: "unsubscribed" },
+  { id: uuid(), label: "dropdown" },
+  { id: uuid(), label: "drawing" }
+];
+
 export const questionnaireReducer = (state, action) => {
   switch (action.type) {
+    case "REORDER":
+      return reorder(state, action.source.index, action.destination.index);
+    case "CLONE":
+      return copy(QUESTION_TYPES, state, action.source, action.destination);
+    case "SET_QUESTIONS":
+      return action.questions;
     case "ADD_QUESTION":
       const uniq = uuid();
       return [
