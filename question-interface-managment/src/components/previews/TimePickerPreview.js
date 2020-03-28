3f2c135a350e1f027b94ab54f89c5e4fccb74a95
Mahir Hiro <mahirhiro@gmail.com>
2020-03-28 21:10:23 +0400

    DatePickerPreview.js

--- question-interface-managment/src/components/previews/TimePickerPreview.js
+++ question-interface-managment/src/components/previews/TimePickerPreview.js
@@ -2,14 +2,13 @@ import 'date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from "@date-io/date-fns"
-import './draw.css';
 import {
     MuiPickersUtilsProvider,
-    KeyboardDatePicker,
+    KeyboardTimePicker,
 } from '@material-ui/pickers';
 
-export default function DatePickerPreview() {
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2020-04-18T21:11:54'));
+export default function TimerPickerPreview() {
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
     const handleDateChange = date => {
         setSelectedDate(date);
@@ -18,17 +17,14 @@ export default function DatePickerPreview() {
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <Grid container justify="space-around">
-                <KeyboardDatePicker
-                    disableToolbar
-                    variant="inline"
-                    format="MM/dd/yyyy"
+                <KeyboardTimePicker
                     margin="normal"
-                    id="date-picker-inline"
-                    label="Date picker inline"
+                    id="time-picker"
+                    label="Time picker"
                     value={selectedDate}
                     onChange={handleDateChange}
                     KeyboardButtonProps={{
-                        'aria-label': 'change date',
+                        'aria-label': 'change time',
                     }}
                 />
             </Grid>
