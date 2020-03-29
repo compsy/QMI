1deacff0da88ccad979705df31ea52b29ce2a78d
Pal Poshyachinda <palp@Pals-MacBook-Pro.local>
2020-03-29 11:02:57 +0700

    edited renderClone function in QuestionsPage.js

--- question-interface-managment/src/components/QuestionsPage.js
+++ question-interface-managment/src/components/QuestionsPage.js
@@ -1,23 +1,37 @@
-import React, {useContext} from "react";
-import {v1 as uuidv1} from "uuid";
-import {Box, Container, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography} from "@material-ui/core";
-import {QuestionnaireContext} from "../contexts/QuestionnaireContext";
+import React, { useContext } from "react";
+import { v1 as uuidv1 } from "uuid";
+import {
+  Box,
+  Container,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  ExpansionPanelSummary,
+  Grid,
+  Paper
+} from "@material-ui/core";
+import { QuestionnaireContext } from "../contexts/QuestionnaireContext";
 import Question from "./Question";
 import ToggleGridAreasButton from "./buttons/ToggleGridAreasButton";
 import AddQuestionButton2 from "./buttons/AddQuestionButton2";
 import StringifiedJSONCard from "./StringifiedJSONCard";
-import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
-import {QUESTION_TYPES} from "./QuestionTypes";
-import './index.css';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { QUESTION_TYPES } from "./QuestionTypes";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import "./index.css";
 
 const QuestionsPage = () => {
-    const {questions, dispatch} = useContext(QuestionnaireContext);
-    const onDragEnd = React.useCallback(result => {
-        const {source, destination} = result;
+  const { questions, dispatch } = useContext(QuestionnaireContext);
+  const onDragEnd = React.useCallback(result => {
+    const { source, destination } = result;
 
-        if (!destination) {
-            return;
-        }
+    if (!destination) {
+      return;
+    }
 
     switch (source.droppableId) {
       case "BAG":
@@ -62,24 +76,24 @@ const BottomSection = ({ items }) => {
         Questions
       </Typography>
       <Droppable droppableId="BAG">
-          {(provided, snapshot) => (
-              <Box ref={provided.innerRef} className="shopping-bag">
-                  {questions.map((question, index) => (
-                      <Draggable
-                          key={question.id}
-                          draggableId={question.id}
-                          index={index}
-                      >
-                          {(provided, snapshot) => (
-                              <div
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                  style={provided.draggableProps.style}
-                              >
-                                  <Question key={uuidv1()} question={question}/>
-                              </div>
-                          )}
+        {(provided, snapshot) => (
+          <Box ref={provided.innerRef} className="shopping-bag">
+            {questions.map((question, index) => (
+              <Draggable
+                key={question.id}
+                draggableId={question.id}
+                index={index}
+              >
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={provided.draggableProps.style}
+                  >
+                    <Question key={uuidv1()} question={question} />
+                  </div>
+                )}
               </Draggable>
             ))}
             {provided.placeholder}
@@ -95,20 +109,23 @@ const BottomSection = ({ items }) => {
 const getRenderItem = (items, className) => (provided, snapshot, rubric) => {
   const item = items[rubric.source.index];
   return (
-    <div>
-      <li
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-        style={provided.draggableProps.style}
-        className={snapshot.isDragging ? "dragging" : ""}
-      >
-        {JSON.stringify(item)}
-      </li>
+    <div
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      ref={provided.innerRef}
+      style={provided.draggableProps.style}
+      // className={snapshot.isDragging ? "dragging" : ""}
+    >
+      <Grid container direction="column">
+        <Grid item xs justify="center" alignItems="stretch" style={{textAlign:"center"}}>
+          <Paper>
+            <Typography variant="h5">{`new ${item.label}`}</Typography>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
-
 
 const Toolbar = ({ items }) => {
   return (
@@ -118,23 +135,29 @@ const Toolbar = ({ items }) => {
       isDropDisabled={true}
     >
       {(provided, snapshot) => (
-          <Drawer ref={provided.innerRef} style={{width: 50}} variant="permanent" anchor="right">
-              <List>
-                  {items.map((item, index) => {
-                      const shouldRenderClone =
-                          item.id === snapshot.draggingFromThisWith;
-                      return (
-                          <React.Fragment style={{textAlign: 'left'}} key={item.id}>
-                              {shouldRenderClone ? (
-                                  <ListItem button key={item.label}>
-                                      <ListItemIcon>
-                                          {item.icon}
-                                      </ListItemIcon>
-                                      <ListItemText primary={item.label} style={{textAlign: 'left'}}/>
-                                  </ListItem>
-                              ) : (
-                                  <Draggable draggableId={item.id} index={index}>
-                                      {(provided, snapshot) => (
+        <Drawer
+          ref={provided.innerRef}
+          style={{ width: 50 }}
+          variant="permanent"
+          anchor="right"
+        >
+          <List>
+            {items.map((item, index) => {
+              const shouldRenderClone =
+                item.id === snapshot.draggingFromThisWith;
+              return (
+                <React.Fragment style={{ textAlign: "left" }} key={item.id}>
+                  {shouldRenderClone ? (
+                    <ListItem button key={item.label}>
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        primary={item.label}
+                        style={{ textAlign: "left" }}
+                      />
+                    </ListItem>
+                  ) : (
+                    <Draggable draggableId={item.id} index={index}>
+                      {(provided, snapshot) => (
                         <div
                           key={item.label}
                           ref={provided.innerRef}
@@ -145,9 +168,7 @@ const Toolbar = ({ items }) => {
                             button
                             className={snapshot.isDragging ? "dragging" : ""}
                           >
-                            <ListItemIcon>
-                              {item.icon}
-                            </ListItemIcon>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
                             <ListItemText primary={item.label} />
                           </ListItem>
                         </div>
