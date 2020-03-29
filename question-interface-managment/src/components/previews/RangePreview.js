b922ba7f6068144ee3df1f96afc350761478f94f
reyrobs <robertrey16@gmail.com>
2020-03-29 18:32:55 +0200

    Updated imports

--- question-interface-managment/src/components/previews/RangePreview.js
+++ question-interface-managment/src/components/previews/RangePreview.js
@@ -1,33 +1,70 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+
+import React, {useContext} from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles(theme => ({
     root: {
-        width: 300,
+        width: 300 + theme.spacing(3) * 2,
     },
-});
+    margin: {
+        height: theme.spacing(3),
+    },
+}));
 
-function RangePreview() {
+
+const PrettoSlider = withStyles({
+    root: {
+        color: '#52af77',
+        height: 8,
+    },
+    thumb: {
+        height: 24,
+        width: 24,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        marginTop: -8,
+        marginLeft: -12,
+        '&:focus, &:hover, &$active': {
+            boxShadow: 'inherit',
+        },
+    },
+    active: {},
+    valueLabel: {
+        left: 'calc(-50% + 4px)',
+    },
+    track: {
+        height: 8,
+        borderRadius: 4,
+    },
+    rail: {
+        height: 8,
+        borderRadius: 4,
+    },
+})(Slider);
+
+
+
+export default function RangePreview({ question }) {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <Typography id="discrete-slider" gutterBottom>
-                Dummy Text
-            </Typography>
-            <Slider
-                defaultValue={30}
-                aria-labelledby="discrete-slider"
-                valueLabelDisplay="auto"
-                step={1}
-                marks
-                min={0}
-                max={100}
+            <Typography gutterBottom>Pretto</Typography>
+            <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider"
+                          defaultValue={30}
+                          aria-labelledby="discrete-slider"
+                          step={question.step}
+                          marks
+                          min={question.min}
+                          max={question.max}
+                          title = {question.title}
+                          // tooltip = {question.tooltip}
+                          labels = {question.labels}
+                          section_end = {question.section_end}
             />
         </div>
     );
 }
-
-export default RangePreview;
