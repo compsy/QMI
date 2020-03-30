6b70c6cd3b87f7f1e0c48c138da04b621754ca87
Pal Poshyachinda <palp@Pals-MacBook-Pro.local>
2020-03-30 19:59:09 +0700

    fixed dnd summary

--- question-interface-managment/src/components/Question.js
+++ question-interface-managment/src/components/Question.js
@@ -1,15 +1,15 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import {
-    Divider,
-    ExpansionPanel,
-    ExpansionPanelDetails,
-    ExpansionPanelSummary,
-    Grid,
-    Typography
+  Divider,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  Grid,
+  Typography
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import {QuestionnaireContext} from "../contexts/QuestionnaireContext";
-import {SettingsContext} from "../contexts/SettingsContext";
+import { QuestionnaireContext } from "../contexts/QuestionnaireContext";
+import { SettingsContext } from "../contexts/SettingsContext";
 import RemoveQuestionButton from "./buttons/RemoveQuestionButton";
 import EditQuestionButton from "./buttons/EditQuestionButton";
 import RadioCheckboxPreview from "./previews/RadioCheckboxPreview";
@@ -22,182 +22,208 @@ import DatePickerPreview from "./previews/DatePickerPreview";
 import TimePickerPreview from "./previews/TimePickerPreview";
 import TextFieldPreview from "./previews/TextFieldPreview";
 import DrawingPreview from "./previews/DrawingPreview";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "./index.css";
 
-const Question = ({question, ...props}) => {
-    const [open, setOpen] = useState(false);
+const Question = ({ index, question, ...props }) => {
+  const [open, setOpen] = useState(false);
 
-    return (
-        <ExpansionPanel expanded={open} {...props}>
-            <Summary onClick={() => setOpen(!open)} question={question}/>
-            <Divider/>
-            <Details question={question}/>
-        </ExpansionPanel>
-    );
+  return (
+    <ExpansionPanel expanded={open} {...props}>
+      <Draggable key={question.id} draggableId={question.id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            style={provided.draggableProps.style}
+          >
+            <Summary onClick={() => setOpen(!open)} question={question} />
+          </div>
+        )}
+      </Draggable>
+      <Divider />
+      <Details question={question} />
+    </ExpansionPanel>
+  );
 };
 
 const style = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
 };
 
 export default Question;
 
-const Summary = ({question, ...props}) => {
-    const {settings} = useContext(SettingsContext);
-    const {questions} = useContext(QuestionnaireContext);
+const Summary = ({ question, ...props }) => {
+  const { settings } = useContext(SettingsContext);
+  const { questions } = useContext(QuestionnaireContext);
 
-    return (
-        <ExpansionPanelSummary {...props}>
-            <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-                spacing={1}
-                style={{
-                    background: settings.showGridAreas ? "lightgrey" : "transparent",
-                    opacity: settings.showGridAreas ? "0.8" : "1.0"
-                }}
-            >
-                <Grid
-                    item
-                    xs
-                    style={{
-                        textAlign: "left",
-                        background: settings.showGridAreas ? "lightgreen" : "transparent",
-                        opacity: settings.showGridAreas ? "0.8" : "1.0"
-                    }}
-                >
-                    <Typography variant="h5">
-                        {questions.indexOf(question) + 1}
-                    </Typography>
-                </Grid>
-                <Grid
-                    item
-                    xs={8}
-                    style={{
-                        textAlign: "left",
-                        wordWrap: "break-word",
-                        wordBreak: "break-word",
-                        background: settings.showGridAreas ? "lightblue" : "transparent",
-                        opacity: settings.showGridAreas ? "0.8" : "1.0"
-                    }}
-                >
-                    <Typography variant="h5">{question.title}</Typography>
-                </Grid>
-                <Grid
-                    item
-                    xs
-                    style={{
-                        textAlign: "right",
-                        background: settings.showGridAreas ? "lightcoral" : "transparent",
-                        opacity: settings.showGridAreas ? "0.8" : "1.0"
-                    }}
-                >
-                    <ExpandMoreIcon />
-                </Grid>
-            </Grid>
-        </ExpansionPanelSummary>
-    );
+  return (
+    <ExpansionPanelSummary {...props}>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={1}
+        style={{
+          background: settings.showGridAreas ? "lightgrey" : "transparent",
+          opacity: settings.showGridAreas ? "0.8" : "1.0"
+        }}
+      >
+        <Grid
+          item
+          xs
+          style={{
+            textAlign: "left",
+            background: settings.showGridAreas ? "lightgreen" : "transparent",
+            opacity: settings.showGridAreas ? "0.8" : "1.0"
+          }}
+        >
+          <Typography variant="h5">
+            {questions.indexOf(question) + 1}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={8}
+          style={{
+            textAlign: "left",
+            wordWrap: "break-word",
+            wordBreak: "break-word",
+            background: settings.showGridAreas ? "lightblue" : "transparent",
+            opacity: settings.showGridAreas ? "0.8" : "1.0"
+          }}
+        >
+          <Typography variant="h5">{question.title}</Typography>
+        </Grid>
+        <Grid
+          item
+          xs
+          style={{
+            textAlign: "right",
+            background: settings.showGridAreas ? "lightcoral" : "transparent",
+            opacity: settings.showGridAreas ? "0.8" : "1.0"
+          }}
+        >
+          <ExpandMoreIcon />
+        </Grid>
+      </Grid>
+    </ExpansionPanelSummary>
+  );
 };
 
 const Details = ({ question }) => {
-    const { settings } = useContext(SettingsContext);
+  const { settings } = useContext(SettingsContext);
 
-    return (
-        <ExpansionPanelDetails>
-            <Grid
-                container
-                direction="column"
-                justify="center"
-                alignItems="stretch"
-                spacing={1}
-                style={{
-                    background: settings.showGridAreas ? "lightgrey" : "transparent",
-                    opacity: settings.showGridAreas ? "0.8" : "1.0"
-                }}
-            >
-                <Grid
-                    item
-                    xs
+  return (
+    <ExpansionPanelDetails>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="stretch"
+        spacing={1}
+        style={{
+          background: settings.showGridAreas ? "lightgrey" : "transparent",
+          opacity: settings.showGridAreas ? "0.8" : "1.0"
+        }}
+      >
+        <Grid
+          item
+          xs
+          style={{
+            textAlign: "center",
+            background: settings.showGridAreas ? "lightgreen" : "transparent",
+            opacity: settings.showGridAreas ? "0.8" : "1.0"
+          }}
+        >
+          <Typography variant="caption">
+            {/* {`${question.type} options preview`.toUpperCase()} */}
+            {question.type.toUpperCase()}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs
+          style={{
+            textAlign: "center",
+            background: settings.showGridAreas ? "lightblue" : "transparent",
+            opacity: settings.showGridAreas ? "0.8" : "1.0"
+          }}
+        >
+          {(() => {
+            switch (question.type) {
+              case "radio":
+                return <RadioCheckboxPreview question={question} />;
+              case "checkbox":
+                return <RadioCheckboxPreview question={question} />;
+              case "likert":
+                return <LikertPreview question={question} />;
+              case "range":
+                return (
+                  <div
                     style={{
-                        textAlign: "center",
-                        background: settings.showGridAreas ? "lightgreen" : "transparent",
-                        opacity: settings.showGridAreas ? "0.8" : "1.0"
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center"
                     }}
-                >
-                    <Typography variant="caption">
-                        {/* {`${question.type} options preview`.toUpperCase()} */}
-                        {question.type.toUpperCase()}
-                    </Typography>
-                </Grid>
-                <Grid
-                    item
-                    xs
+                  >
+                    <RangePreview question={question} />
+                  </div>
+                );
+              case "select":
+                return (
+                  <div
                     style={{
-                        textAlign: "center",
-                        background: settings.showGridAreas ? "lightblue" : "transparent",
-                        opacity: settings.showGridAreas ? "0.8" : "1.0"
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center"
                     }}
-                >
-                    {(() => {
-                        switch (question.type) {
-                            case "radio":
-                                return <RadioCheckboxPreview question={question}/>;
-                            case "checkbox":
-                                return <RadioCheckboxPreview question={question}/>;
-                            case "likert":
-                                return <LikertPreview question={question}/>;
-                            case "range":
-                                return <div
-                                    style={{
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                    }}
-                                ><RangePreview question={question}/>
-                                </div>;
-                            case "select":
-                                return <div
-                                    style={{
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                    }}
-                                ><SelectPreview question={question}/>
-                                </div>;
-                            case "textarea":
-                                return <div style={style}><TextArea question={question}/>
-                                </div>;
-                            case "number":
-                                return <NumberPreview question={question}/>;
-                            case "date":
-                                return <DatePickerPreview question={question}/>;
-                            case "time":
-                                return <TimePickerPreview question={question}/>;
-                            case "text field":
-                                return <TextFieldPreview question={question}/>;
-                            case "draw":
-                                return <div style={style}><DrawingPreview question={question}/></div>;
-                            default:
-                                return <RadioCheckboxPreview question={question}/>;
-                        }
-                    })()}
-                </Grid>
-                <Grid
-                    item
-                    xs
-                    style={{
-                        textAlign: "center",
-                        background: settings.showGridAreas ? "lightcoral" : "transparent",
-                        opacity: settings.showGridAreas ? "0.8" : "1.0"
-                    }}
-                >
-                    <RemoveQuestionButton question={question} />
-                    <EditQuestionButton question={question} />
-                </Grid>
-            </Grid>
-        </ExpansionPanelDetails>
-    );
+                  >
+                    <SelectPreview question={question} />
+                  </div>
+                );
+              case "textarea":
+                return (
+                  <div style={style}>
+                    <TextArea question={question} />
+                  </div>
+                );
+              case "number":
+                return <NumberPreview question={question} />;
+              case "date":
+                return <DatePickerPreview question={question} />;
+              case "time":
+                return <TimePickerPreview question={question} />;
+              case "text field":
+                return <TextFieldPreview question={question} />;
+              case "draw":
+                return (
+                  <div style={style}>
+                    <DrawingPreview question={question} />
+                  </div>
+                );
+              default:
+                return <RadioCheckboxPreview question={question} />;
+            }
+          })()}
+        </Grid>
+        <Grid
+          item
+          xs
+          style={{
+            textAlign: "center",
+            background: settings.showGridAreas ? "lightcoral" : "transparent",
+            opacity: settings.showGridAreas ? "0.8" : "1.0"
+          }}
+        >
+          <RemoveQuestionButton question={question} />
+          <EditQuestionButton question={question} />
+        </Grid>
+      </Grid>
+    </ExpansionPanelDetails>
+  );
 };
