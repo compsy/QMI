c510fc85710f4441bd93bc387936ed22cd0beb1c
HNaida <h.shmak@student.rug.nl>
2020-04-19 12:08:06 +0300

    Delete website data button added.

--- question-interface-managment/src/contexts/QuestionnaireContext.js
+++ question-interface-managment/src/contexts/QuestionnaireContext.js
@@ -5,9 +5,24 @@ export const QuestionnaireContext = createContext();
 
 const QuestionnaireContextProvider = (props) => {
   const [questions, dispatch] = useReducer(questionnaireReducer, [
-    {id: 'v1', type: "radio", title: "untitled radio", options: ["option 1", "option 222", "option 3", "option 4"]},
+    {id: 'v1', type: "radio", title: "Bye KPJ", options: ["option 1", "option 222", "option 3", "option 4"]},
     {id: 'v2', type: "checkbox", title: "untitled checkbox", options: ["option 1", "option 2", "option 3", "option 4"]},
-    {id: 'v3', type: "range", title: "untitled range", options: ["option 1", "option 2", "option 3", "option 4"]},
+    {
+      section_start: 'De hoofddoelen',
+      hidden: true,
+      id: 'v3',
+      type: "range",
+      min: 0,
+      max: 200,
+      step: 5,
+      title: 'Was het voor jou duidelijk ?',
+      tooltip: 'some tooltip',
+      labels: ['helemaal niet duidelijk', 'heel duidelijk'],
+
+      // only since for debug rendering purposes, delete in final
+      options: ['helemaal niet duidelijk', 'heel duidelijk'],
+      section_end: true
+    },
     {id: 'v4', type: "likert", title: "untitled likert",
       options: [
         { title: 'hobby/sport', numeric_value: 0 },
@@ -38,7 +53,7 @@ const QuestionnaireContextProvider = (props) => {
       type: "range",
       min: 0,
       max: 200,
-      step: 1,
+      step: 10,
       title: 'Was het voor jou duidelijk ?',
       tooltip: 'some tooltip',
       labels: ['helemaal niet duidelijk', 'heel duidelijk'],
