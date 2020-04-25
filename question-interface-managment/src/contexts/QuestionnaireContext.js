7d0675432869b07d18c6738a44fa913fcc7f7ba6
HNaida <h.shmak@student.rug.nl>
2020-04-25 20:17:44 +0300

    RangePreview: fixed min, max, step property; added labels

--- question-interface-managment/src/contexts/QuestionnaireContext.js
+++ question-interface-managment/src/contexts/QuestionnaireContext.js
@@ -6,9 +6,9 @@ export const QuestionnaireContext = createContext();
 const QuestionnaireContextProvider = (props) => {
   const [questions, dispatch] = useReducer(questionnaireReducer, [
     {id: 'v1', type: "range", title: "Hello BOI", labels: ["option 1", "option 2", "option 3", "option 4"]},
-    {id: 'v12', type: "radio", title: "Hello Kitty", options: ["option 1", "option 2", "option 3", "option 4"]},
-    {id: 'v2', type: "checkbox", title: "untitled checkbox", options: ["option 1", "option 2", "option 3", "option 4"]},
-
+    {id: 'v2', type: "radio", title: "Hello Kitty", options: ["option 1", "option 2", "option 3", "option 4"]},
+    {id: 'v3', type: "checkbox", title: "untitled checkbox", options: ["option 1", "option 2", "option 3", "option 4"]},
+    {id: 'v4', type: "range", title: "Hey Arnold", labels: ["passing rate"]},
   ]);
   return (
     <QuestionnaireContext.Provider value={{questions, dispatch}}>
