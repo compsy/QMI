2e9487f29a72d241bcb356dbdeb3a1683c50060b
mahirhiro <mahirhiro@gmail.com>
2020-04-27 20:16:38 +0400

    Added toggle switch for theme changer

--- question-interface-managment/src/App.js
+++ question-interface-managment/src/App.js
@@ -1,36 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import SettingsContextProvider from "./contexts/SettingsContext";
 import QuestionnaireContextProvider from "./contexts/QuestionnaireContext";
 import QuestionsPage from "./components/QuestionsPage";
 import "./components/background.css";
-// import "typeface-roboto";
-import useTheme from "./hooks/useTheme";
-import { ThemeProvider } from "@material-ui/core";
+import {Switch, FormControlLabel, Toolbar, Typography, AppBar, makeStyles} from "@material-ui/core";
+import {createMuiTheme, MuiThemeProvider, ThemeProvider} from "@material-ui/core";
+
+const themeObject = {
+    palette: {
+        type: 'light'
+    }
+};
+const useDarkMode = () => {
+    const [theme, setTheme] = useState(themeObject);
+    const {palette : {type}} = theme;
+    const toggleDarkMode = () => {
+        const updatedTheme = {
+            ...theme,
+            palette: {
+                ...theme.palette,
+                type: type === 'light' ? 'dark' : 'light'
+            }
+        };
+        setTheme(updatedTheme);
+    };
+    return [theme, toggleDarkMode]
+};
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: "white",
+        alignItems: "center",
+        background: 'linear-gradient(45deg, #7c4dff 30%, #80deea 90%)',
+    }
+}));
 
 function App() {
-  const theme = useTheme();
+    const [theme, toggleDarkMode] =  useDarkMode();
+    const themeConfig = createMuiTheme(theme);
+    const classes = useStyles();
+    return (
+        <MuiThemeProvider  theme={themeConfig}>
+            <div style={{display: "flex", flexDirection: "column"}}>
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h4" noWrap>
+                        Questionnaire Editor
+                        <FormControlLabel control = {<Switch onClick = {toggleDarkMode} />}/>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <SettingsContextProvider>
-            <QuestionnaireContextProvider>
-              <QuestionsPage />
-            </QuestionnaireContextProvider>
-          </SettingsContextProvider>
-        </div>
-      </div>
-    </ThemeProvider>
-  );
+                <SettingsContextProvider>
+                    <QuestionnaireContextProvider>
+                        <QuestionsPage />
+                    </QuestionnaireContextProvider>
+
+                </SettingsContextProvider>
+            </div>
+
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
