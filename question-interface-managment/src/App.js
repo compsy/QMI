9a711647d480979fd03a16ec76d0e23ae6b67853
Pal Poshyachinda <palp@Pals-MacBook-Pro.local>
2020-04-27 15:46:59 +0700

    major refactor editdialog2

--- question-interface-managment/src/App.js
+++ question-interface-managment/src/App.js
@@ -2,29 +2,35 @@ import React from "react";
 import SettingsContextProvider from "./contexts/SettingsContext";
 import QuestionnaireContextProvider from "./contexts/QuestionnaireContext";
 import QuestionsPage from "./components/QuestionsPage";
-import './components/background.css'
+import "./components/background.css";
 import "typeface-roboto";
+import useTheme from "./hooks/useTheme";
+import { ThemeProvider } from "@material-ui/core";
 
 function App() {
-    return (
-        <div className="App">
-            <div
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%"
-                }}
-            >
-                <SettingsContextProvider>
-                    <QuestionnaireContextProvider>
-                        <QuestionsPage />
-                    </QuestionnaireContextProvider>
-                </SettingsContextProvider>
-            </div>
+  const theme = useTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <SettingsContextProvider>
+            <QuestionnaireContextProvider>
+              <QuestionsPage />
+            </QuestionnaireContextProvider>
+          </SettingsContextProvider>
         </div>
-    );
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
