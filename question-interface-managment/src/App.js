b25b3e1e7b9d284282aa59a9d9119c4a22d8351d
mahirhiro <mahirhiro@gmail.com>
2020-04-28 01:16:16 +0400

    1. Added new class for sidebar 2. Added option to toggle for theme 3. Added SearchBar.js if anyone want to continue the filter

--- question-interface-managment/src/App.js
+++ question-interface-managment/src/App.js
@@ -3,8 +3,12 @@ import SettingsContextProvider from "./contexts/SettingsContext";
 import QuestionnaireContextProvider from "./contexts/QuestionnaireContext";
 import QuestionsPage from "./components/QuestionsPage";
 import "./components/background.css";
-import {Switch, FormControlLabel, Toolbar, Typography, AppBar, makeStyles} from "@material-ui/core";
-import {createMuiTheme, MuiThemeProvider, ThemeProvider} from "@material-ui/core";
+import {AppBar, createMuiTheme, makeStyles, MuiThemeProvider, Toolbar, Typography} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import {AntSwitch} from './AntSwitch';
+import SearchBar from "./SearchBar";
 
 const themeObject = {
     palette: {
@@ -13,7 +17,7 @@ const themeObject = {
 };
 const useDarkMode = () => {
     const [theme, setTheme] = useState(themeObject);
-    const {palette : {type}} = theme;
+    const {palette: {type}} = theme;
     const toggleDarkMode = () => {
         const updatedTheme = {
             ...theme,
@@ -27,30 +31,48 @@ const useDarkMode = () => {
     return [theme, toggleDarkMode]
 };
 const useStyles = makeStyles(theme => ({
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
         color: "white",
-        alignItems: "center",
+        textAlign: "center",
         background: 'linear-gradient(45deg, #7c4dff 30%, #80deea 90%)',
-    }
+    },
 }));
 
 function App() {
     const [theme, toggleDarkMode] =  useDarkMode();
     const themeConfig = createMuiTheme(theme);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const classes = useStyles();
     return (
         <MuiThemeProvider  theme={themeConfig}>
             <div style={{display: "flex", flexDirection: "column"}}>
             <AppBar className={classes.appBar}>
                 <Toolbar>
-                    <Typography variant="h4" noWrap>
-                        Questionnaire Editor
-                        <FormControlLabel control = {<Switch onClick = {toggleDarkMode} />}/>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Questionnaire Interface
                     </Typography>
+                    <Typography component="div">
+                        <Grid component="label" container alignItems="center" spacing={1}>
+                            <Grid item>Light Mode</Grid>
+                            <Grid item>
+                                <AntSwitch onChange={toggleDarkMode} value="checkedC"/>
+                            </Grid>
+                            <Grid item>Dark Mode</Grid>
+                        </Grid>
+                    </Typography>
+                    <SearchBar/>
                 </Toolbar>
             </AppBar>
-
                 <SettingsContextProvider>
                     <QuestionnaireContextProvider>
                         <QuestionsPage />
