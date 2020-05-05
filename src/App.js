b35a6de8c1ac851c9e03bf7fc61770c717345857
reyrobs <robertrey16@gmail.com>
2020-05-05 16:25:02 +0200

    added login/logout button

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import { AntSwitch } from "./AntSwitch";
 import "./background.css";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import nav from './components/buttons/nav'
+import Nav from './components/buttons/nav'
 
 const themeObject = {
   palette: {
@@ -68,7 +68,7 @@ function App() {
           <div style={{ display: "flex", flexDirection: "column" }}>
             <AppBar className={classes.appBar}>
               <Toolbar>
-                <nav/>
+                <Nav/>
                 <IconButton
                   edge="start"
                   className={classes.menuButton}
