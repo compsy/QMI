9fd265ee73bc0c9ecb98a276619942a78110ca67
HNaida <h.shmak@student.rug.nl>
2020-05-20 15:30:00 +0300

    QMI Tutorial: window size fixed.

--- src/WatchTutorial.js
+++ src/WatchTutorial.js
@@ -1,8 +1,10 @@
 import React, { useCallback, useState } from 'react'
 import './scroll.css';
-import ModalVideo from 'react-modal-video'
+import ModalVideo from 'react-modal-video';
+import { Resizable, ResizableBox } from 'react-resizable';
 
 const WatchTutorial = ({isOpen, toggleModal}) => {
+    const ResizableBox = require('react-resizable').ResizableBox;
 
     const opts = {
         height: '10',
@@ -13,7 +15,9 @@ const WatchTutorial = ({isOpen, toggleModal}) => {
     };
 
     return (
-        <ModalVideo channel='youtube' isOpen={isOpen} opts={opts}  videoId='C1-fRwmvzng' onClose={toggleModal} />
+        <ResizableBox width={400} height={100}>
+            <ModalVideo channel='youtube' ratio='2000:10' isOpen={isOpen} opts={opts}  videoId='C1-fRwmvzng' onClose={toggleModal} />
+        </ResizableBox>
     )
 };
 
